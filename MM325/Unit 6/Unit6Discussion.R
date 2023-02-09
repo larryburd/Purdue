dd975db1dbb71b47dd42ef10f237abfc41a51235
Laurence Burden <laurenceburden@gmail.com>
2023-02-08 21:20:20 -0600

    added file meta information

--- MM325/Unit 6/Unit6Discussion.R
+++ MM325/Unit 6/Unit6Discussion.R
@@ -1,6 +1,18 @@
+############################################################################
+# Title:  Unit6Discussion.R
+# Author: Laurence Burden
+# Date:   8 Feb 2023
+#
+# Purpose: Perform ANOVA test of question 50 and
+#          question 262 of WVS data
+# H.0: mu.1 = mu.2 = mu.3 = mu.4 = mu.5 = mu.6 = mu.7 = mu.8 = mu.9 = mu.10 
+# H.a: not all means are equal
+############################################################################
+
+# load tidyverse to make manipulating data easier
 library(tidyverse)
 
-# Load WVS file
+# Load World Values Survey data
 folderPath <- "/home/larryburd/Documents/"
 fileName <- "WVS_Cross-National_Wave_7_rData_v5_0.rdata"
 filePath <- paste(folderPath, fileName, sep = "")
