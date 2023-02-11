a04414a84585e7fab0027452295ca262d2b0a240
Laurence Burden <laurenceburden@gmail.com>
2023-02-11 12:36:52 -0600

    updated unit6.R

--- MM325/Unit 6/Unit6Discussion.R
+++ MM325/Unit 6/Unit6Discussion.R
@@ -1,11 +1,11 @@
 ############################################################################
 # Title:  Unit6Discussion.R
 # Author: Laurence Burden
-# Date:   8 Feb 2023
+# Date:   10 Feb 2023
 #
-# Purpose: Perform ANOVA test of question 50 and
+# Purpose: Perform ANOVA test of question 51 and
 #          question 262 of WVS data
-# H.0: mu.1 = mu.2 = mu.3 = mu.4 = mu.5 = mu.6 = mu.7 = mu.8 = mu.9 = mu.10 
+# H.0: mu.1 = mu.2 = mu.3 = mu.4
 # H.a: not all means are equal
 ############################################################################
 
@@ -13,19 +13,43 @@
 library(tidyverse)
 
 # Load World Values Survey data
+# Break file path apart to stay under 80 chars
 folderPath <- "/home/larryburd/Documents/"
 fileName <- "WVS_Cross-National_Wave_7_rData_v5_0.rdata"
 filePath <- paste(folderPath, fileName, sep = "")
 load(filePath)
 
-# Remove responses at 0 or lower to question 50
+# Remove responses at 0 or lower to question 51
 # and question 262
 WVS <- `WVS_Cross-National_Wave_7_v5_0` |>
-  filter(Q50 > 0 & Q262 > 0)
+  filter(Q51 > 0 & Q262 > 0)
+
+# Response levels summaries
+for (n in 1:4) {
+  # Filter for response level n
+  rLvl <- WVS |>
+    filter(Q51 == n)
+  
+  # Get all ages for this level and summary stats
+  rAges <- rLvl$Q262
+  aCount <- length(rAges)
+  aSum <- sum(rAges)
+  aMu <- mean(rAges)
+  aVar <- var(rAges)
+  
+  # put it all in a data frame to make a prettier print
+  df <- data.frame(Response_lvl = n, Count = aCount, 
+                   Sum = aSum, Mean = aMu, Variance = aVar)
+  
+  # Print summary stats
+  print(df)
+}
 
 # Perform ANOVA test against questions 262 (age) and
-# question 50 (Satisfaction with financial situation)
-anovaResult <- aov(Q262 ~ Q50, data = WVS)
+# question 50 (Frequency of going without food in last 12 months)
+# Age (Q262) is our continuous/dependent variable and question response (Q51)
+# is the categorical/independent variable
+anovaResult <- aov(Q262 ~ Q51, data = WVS)
 
 # Display results
 summary(anovaResult)
