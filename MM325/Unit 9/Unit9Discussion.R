686c34a4f4ae29134756ac021be8809e1f706a88
Laurence Burden <laurenceburden@gmail.com>
2023-03-07 12:40:32 -0600

    added discussion response files

--- MM325/Unit 9/Unit9Discussion.R
+++ MM325/Unit 9/Unit9Discussion.R
@@ -1,11 +1,14 @@
 #####################################################################
 # title: Unit9Discussion.R
 # Author: Laurence Burden
-# Date: 20230304
-# Purpose: Create text analysis techniques
+# Date: 5Mar2023
+# Purpose: Utilize text analysis techniques
 #
 # Note: must run install.packages("textdata") and 
 #       accept NRC's terms to fully run this script
+#
+# Note: Text must be saved without any carriage returns or line feeds
+#       to execute properly
 #
 # Citation: Sentiment Analysis: Automatically Detecting Valence, 
 #             Emotions, and Other Affectual States from Text. Saif 
@@ -13,42 +16,9 @@
 #####################################################################
 # Load libraries for storing and analyzing data
 library(tidyverse)
-library(tidytext)
 library(reshape2)
+library(tidytext)
 library(readr)
-
-# Global to hold plot
-articleBarGraph <- NULL
-
-main <- function() {
-  # Get text from file
-  filePath <- paste("/Users/laure/OneDrive/Documents",
-                    "/Purdue/Purdue/MM325/Unit 9/",
-                    "MM325_U9_Text1.txt",
-                    sep = "")
-  filePath <- paste("/home/larryburd/Documents/",
-                    "Purdue/MM325/Unit 9/",
-                    "MM325_U9_Text1.txt",
-                    sep = "")
-  ?read_delim
-  messyText <- read_delim(filePath, delim = "\n")
-  messyText <- tibble(line = 1:6172, text = messyText[1])
-  messyText
-  # Transform data into tidy text format
-  tidyArticles <- messyText |>
-    mutate_all(as.character) |>
-    unnest_tokens(word, text)
-  
-  # Load "stop word" to remove
-  data("stop_words")
-  
-  # Remove words such as "the", "of", "to", etc
-  tidyArticles <- tidyArticles |>
-    anti_join(stop_words)
-  
-  # Create plot and add them to the global variables
-  articleBarGraph <<- plotSentimentsBarGraph(tidyArticles, articleTitles) 
-}
 
 # Function to retrieve sentiment analysis based on provided
 # lexicon (lex), sentiment (s), and tidytext formatted vector (tText)
@@ -65,42 +35,63 @@ getSentimentCount <- function(lex, s, tText) {
   return(wordCount)
 }
 
-# Function to plot line graph of NCR sentiments for given tidy text vector
-# (tArticles) and list of titles (titles)
-plotSentimentsLineGraph <- function(articles) {
+# Function to plot bar graph of NCR sentiments for given tidy text vector
+plotSentimentsBarGraph <- function(articles) {
   # Sentiment values for NRC lexicon
   senTypes <- c("trust", "fear", "negative", "sadness", "anger",
                 "surprise", "positive", "disgust", "joy", "anticipation")
   # initialize an empty vector to hold all sentiment counts
   senCounts <- c()
   
-  # Get word count for each sentiment and add it to sentiment count vector
+  # Tibble to hold all the values of the sentiment counts
+  artSentiments <- tibble(negative = "",
+                          sadness = "", anger = "",
+                          disgust = "", fear = "",
+                          positive = "", trust = "",
+                          surprise = "", joy = "",
+                          anticiptation = "") 
+  
+  # Get word count for each sentiment and add it to sentiment count tibble
   for (sen in senTypes) {
     wordCount <- getSentimentCount("nrc", sen, articles)
-    senCounts <- c(senCounts, sum(wordCount$n))
+    artSentiments[sen] <- sum(wordCount$n)
   }
   
+  # Melt data to enable easier plotting
+  meltedData <- melt(artSentiments)
   
-  # Gather all data together in a tibble. Each sentiment has 4 counts, one 
-  # for each article
-  artSentiments <- tibble(negative = senCounts[9:12],
-                          sadness = senCounts[13:16], anger = senCounts[17:20],
-                          disgust = senCounts[29:32], fear = senCounts[5:8],
-                          positive = senCounts[25:28], trust = senCounts[1:4],
-                          surprise = senCounts[21:24], joy = senCounts[33:36],
-                          anticiptation = senCounts[37:40]) 
-  
-  # melt data down to create named variables and value counts for plotting
-  #meltedData <- melt(artSentiments, id.vars = "title")
-  
-
-  # Bar chart showing positive and negative word counts per article
-  ggplot(data = artSentiments, aes(y = value, fill = variable)) +
+  # Bar chart showing sentiment word counts
+  ggplot(data = meltedData, aes(x = variable, y = value, fill = variable)) +
     geom_bar(stat="identity", position = "dodge") +
-    labs(title = "Sentiments for Student Debt Articles", 
-         x = "Sentiments", y = "Word Count")
+    labs(title = "Sentiment Analysis for Unit 9 Discussion", 
+         x = "Sentiment", y = "Word Count")
 }
 
-# Run program and display plot
-main()
-articleBarGraph
+
+# ****MAIN FUNCTION SECTION****
+# Get text from file
+winFilePath <- paste("/Users/laure/OneDrive/Documents",
+                     "/Purdue/Purdue/MM325/Unit 9/",
+                     "MM325_U9_Text1.txt",
+                     sep = "")
+messyText <- read_file(winFilePath)
+
+# Split the words into individual rows
+articleText <- strsplit(messyText, " +")
+
+# Unnest articleText to a tibble for use with TidyText
+artTibble <- tibble(line = 1:length(articleText[[1]]), text = articleText[[1]])
+
+# Reformat text into TidyText friendly format
+tidyArticles <- artTibble |>
+  unnest_tokens(word, text)
+
+# Load "stop word" to remove
+data("stop_words")
+
+# Remove words such as "the", "of", "to", etc
+tidyArticles <- tidyArticles |>
+  anti_join(stop_words)
+
+# Call the sentiment function
+plotSentimentsBarGraph(tidyArticles)
