99aa008e9c2e41858f0826338e09ad9eed48bd53
laurence burden <laurenceburden@gmail.com>
2023-03-09 20:46:33 -0600

    Finalized U10 and added discussion file

--- MM325/Unit 10/Unit10.R
+++ MM325/Unit 10/Unit10.R
@@ -1,9 +1,9 @@
 # BINOMIAL DISTRIBUTIONS -----------------------------------------
 # Binomial Distribution for x
-p <- .9
-n <- 19
-x <- 16
-
+p <- .4
+n <- 12
+x <- 10
+493646 * .1584
 # Expected Value of X, variance, and standard deviation
 expVal <- n * p
 variance <- expVal * (1 - p)
@@ -32,11 +32,8 @@ pbinom(x, size = n, prob = p)
 pbinom(x, size = n, prob = p, lower.tail = FALSE)
 
 # CHI-SQUARE ----------------------------------------------------
-a <- .05
-df <- 6
-
-# Left sided chi-square test critical value
-qchisq(a, df)
+a <- .025
+df <- 1
 
 # Right sided chi-square test critical value
 round(qchisq(a, df, lower.tail = FALSE), digits =3)
@@ -55,7 +52,8 @@ popVar <- 5400^2
 
 # Print the stats of the vector
 dfStats <- data.frame(DegreesOfFreedom = df, SampleVariance = smplVar,
-                      SampleStandardDev = smplStDev, PopulationVariance = popVar)
+                      SampleStandardDev = smplStDev, 
+                      PopulationVariance = popVar)
 dfStats
 # Find Chi-Square Statistic
 chi <- (df * smplVar) / popVar
@@ -67,8 +65,8 @@ var_pop <- function(x) {
 
 # Conduct a chi-square test for goodness of fit given a vector of values
 # and an equal probability of for each value
-x <- c(85, 38, 107)
-p <- c(.3, .2, .5)
+x <- c(17,19,23,21,10)
+p <- 1/5
 a <- 0.025
 chiGoF(a, x, p, printVars = TRUE)
 vP <- rep(p, length(x))
@@ -77,8 +75,6 @@ res <- chisq.test(x, p = vP)
 res$statistic
 res$expected[1]
 
-161 * 1/12
-1/12
 # Function to determine if H0 should be rejected based on a given alpha level a,
 # vector of values x, and a given probability p.  
 chiGoF <- function(a, x, p, printVars = FALSE) {
@@ -137,7 +133,8 @@ cat("Chi-squared test for association:\n")
 print(result)
 
 # Create a matrix to hold the data
-data <- matrix(c(18, 64, 20, 23, 30, 49, 14, 18, 45, 61, 20, 22), nrow = 3, byrow = TRUE)
+data <- matrix(c(18, 64, 20, 23, 30, 49, 14, 18, 45, 61, 20, 22), 
+               nrow = 3, byrow = TRUE)
 colnames(data) <- c("Favorable", "Unfavorable", "Neutral")
 rownames(data) <- c("18-30", "30-45", "Over 45")
 # Perform the chi-squared test for association
@@ -147,3 +144,17 @@ result$expected
 cat("Chi-squared test for association:\n")
 print(result)
 
+values <- c(61,69,57,78)
+data <- matrix(values, nrow = 2, byrow = TRUE)
+rownames(data) <- c("Diseased", "Not Diseased")
+colnames(data) <- c("Vaccinated", "Not Vaccinated")
+
+values <- c(60,50,52,20,16,18,17,16,16)
+data <- matrix(values, nrow = 3, byrow = TRUE)
+rownames(data) <- c("Favorable", "Unfavorable", "Neutral")
+colnames(data) <- c("18-30", "30-45", "Over 45")
+
+data
+res <- chisq.test(data)
+res$expected
+res
